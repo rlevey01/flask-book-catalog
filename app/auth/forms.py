a44b50a5e75930cdf6e28316923c02598d964efe
rlevey01 <rlevey01@gmail.com>
2019-08-29 16:28:48 -0500

    flask-advanced

---
+++ app/auth/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.auth.models import User
+
+def user_name_exists(form, field):
+    user_name = User.query.filter_by(user_name=field.data).first()
+    if user_name:
+        raise ValidationError('User Name Already Exists')
+
+
+def email_exists(form, field):
+    email = User.query.filter_by(user_email=field.data).first()
+    if email:
+        raise ValidationError('Email Already Exists')
+
+class RegistraionForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(),
+                                           Length(3,15, message='between 3 to 15 characters'),user_name_exists])
+    email = StringField('E-mail',validators=[DataRequired(), Email(), email_exists])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm = PasswordField('Confirm', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(5), EqualTo('confirm', message='password must match')])
+    confirm = PasswordField('Confirm', validators=[DataRequired()])
+    stay_loggedin = BooleanField('stay logged-in')
+    submit = SubmitField('Login')
+
---
+++ config/dev.py
@@ -0,0 +1,7 @@
+
+DEBUG = True
+SECRET_KEY = 'topsecret'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Yeti2012@localhost/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+#   SQLALCHEMY_DATABASE_URI='<database>://<user_id>:<password>@<server>/<database_name>',
