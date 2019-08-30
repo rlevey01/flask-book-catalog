6ff8c5a548ac430249c4c6b50294eb88b5de25fb
rlevey01 <rlevey01@gmail.com>
2019-08-29 20:48:00 -0500

    fixed integrity error

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app, db
 from app.auth.models import User
+from sqlalchemy import exc
 
 
 
@@ -8,10 +9,10 @@ from app.auth.models import User
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry',
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry',
                          email='harry@potter.com',
                          password='secret)')
-
-
-flask_app.run()
+    except exc.IntegrityError:
+        flask_app.run()
